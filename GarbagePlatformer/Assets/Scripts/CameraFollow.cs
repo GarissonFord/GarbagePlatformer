d6f2833a84714884934a58a4b5f0f22399dedc5b
Gary DiCiano <garydiciano4@gmail.com>
2018-12-20 12:24:31 -0500

    Pretty Confused
    I have a third-person camera that pivots around the player but unfortunately, I'm struggling to get the player to move in the direction the camera is facing, because of this it can resemble the security-camera style of old Resident Evil.

---
+++ GarbagePlatformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float cameraMoveSpeed = 120.0f;
+    //What the camera follows
+    public GameObject CameraFollowObj;
+    Vector3 followPos;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObj;
+    public GameObject playerObj;
+    public float canDistanceXToPlayer;
+    public float canDistanceYToPlayer;
+    public float canDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //We need to setup the rotation of the sticks 
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+	}
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = CameraFollowObj.transform;
+
+        //move towards the target
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
---
+++ GarbagePlatformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float x, z;
+    public float speed;
+
+    public GameObject cameraBase;
+
+    float distance = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(x, 0.0f, z);
+
+        if (z != 0)
+        {
+            float difference = cameraBase.transform.eulerAngles.y - transform.eulerAngles.y;
+            transform.Rotate(0.0f, difference, 0.0f, Space.Self);
+            movement += cameraBase.transform.forward;
+            rb.AddRelativeForce(cameraBase.transform.forward * speed);
+        }
+
+        //rb.AddForce(movement * speed);
+        //rb.MovePosition
+    }
+
+        
+
+    void FixedUpdate()
+    {
+
+    }
+}
